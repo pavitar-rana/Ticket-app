bcbda3b80ed75d47b46b939d9b7ca2e357f1a4ce
Pavitar Rana <ranapavitar14@gmail.com>
2025-06-06 00:30:30 +0530

    initial setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,30 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+export default [
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
 
-export default eslintConfig;
+    {
+        plugins: {
+            "simple-import-sort": simpleImportSort,
+        },
+        rules: {
+            "simple-import-sort/imports": [
+                "error",
+                {
+                    groups: [["^\\u0000", "^node:", "^@?\\w", "^", "^\\."]],
+                },
+            ],
+            "simple-import-sort/exports": "error",
+        },
+    },
+];
